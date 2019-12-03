87428777f39d6f29dc07b34328fa1a03c30dab28
ronjamyhr <ronja_myhr@hotmail.com>
2019-12-03 14:22:15 +0100

    add google map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.scss'
+import GoogleMap from './components/GoogleMap/GoogleMap'
 
 function App() {
   return (
     <div className="App">
       <h1>Hej</h1>
+      <GoogleMap></GoogleMap>
     </div>
   )
 }
---
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import './googleMap.scss'
+import GoogleMapReact from 'google-map-react'
+
+const Marker = ({ text }: any) => <div>{text}</div>
+const Markers = ({ text }: any) => <div>{text}</div>
+
+const GoogleMap = (props: any) => {
+  const [yourPosition, setYourPosition] = useState<any>({
+    lat: null,
+    lng: null,
+  })
+  const [zoom, setZoom] = useState<number>(15)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        setYourPosition({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        })
+      },
+      error => console.log(error)
+    )
+  }, [])
+
+  return (
+    <div className="google-map-container">
+      <h2>Google map</h2>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLE_API_KEY}` }}
+          center={yourPosition}
+          defaultZoom={zoom}
+        >
+          <Marker
+            lat={yourPosition.lat}
+            lng={yourPosition.lng}
+            text="MY POSITION"
+          />
+
+          <Markers lat={59.626338} lng={17.851603} text="PLACES" />
+        </GoogleMapReact>
+      </div>
+    </div>
+  )
+}
+
+export default GoogleMap
