98517492ebd24a88a2ba17d41ba7bc806ff12084
Rebecca Kling <44289454+rebeccakling@users.noreply.github.com>
2019-12-05 10:51:04 +0100

    Feature/login firebase google (#6)
    * Add login system with google and facebook with firebase
    
    * Fixed login with google and facebook and fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
 import React from 'react'
 import './App.scss'
-import {Switch, Route, BrowserRouter} from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home/Home'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
-const App = () => {
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <Home />
-      <Switch>
-        <Route path='/' component={ Home } />
-      </Switch>
-    </div>
-    </BrowserRouter>
-  )
+interface IState {
+  isSignedIn: boolean
+}
+
+class App extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      isSignedIn: false,
+    }
+  }
+
+  uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: () => false,
+    },
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+    })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          {this.state.isSignedIn ? (
+            <React.Fragment>
+              <button onClick={() => firebase.auth().signOut()}>
+                Sign Out!
+              </button>
+              <Route path="/" component={Home} />
+            </React.Fragment>
+          ) : (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )}
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App
