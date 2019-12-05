e9ca167608b080b187bacfcc7dcc78f3bb61a11c
RebeccaKling <kling.rebecca@hotmail.com>
2019-12-05 09:43:06 +0100

    Add login system with google and facebook with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,60 @@
 import React from 'react'
 import './App.scss'
-import {Switch, Route, BrowserRouter} from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home/Home'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
-const App = () => {
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <Home />
-      <Switch>
-        <Route path='/' component={ Home } />
-      </Switch>
-    </div>
-    </BrowserRouter>
-  )
+interface IState {
+  isSignedIn: boolean
+}
+
+class App extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      isSignedIn: false,
+    }
+  }
+
+  uiConfig = {
+    sigInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: () => false,
+    },
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+    })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          {this.state.isSignedIn ? (
+            <div>Signed In</div>
+          ) : (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )}
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App
