02f46f5ca6d7aa0b68ce281852842cc0d93c1a19
RebeccaKling <kling.rebecca@hotmail.com>
2019-12-11 10:15:07 +0100

    logged in user with redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,35 @@ import React from 'react'
 import './App.scss'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home/Home'
-import firebase from 'firebase/app'
 import 'firebase/auth'
 import Auth from './components/Auth/Auth'
+import { IUser } from './types/User'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
 
-// interface IState {
-//   isSignedIn: boolean
-// }
-
-class App extends React.Component<{}, {}> {
-  // constructor(props: any) {
-  //   super(props)
-
-  //   this.state = {
-  //     isSignedIn: false,
-  //   }
-  // }
-
-  // uiConfig = {
-  //   signInFlow: 'popup',
-  //   signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.FacebookAuthProvider.PROVIDER_ID],
-  //   callbacks: {
-  //     signInSuccessWithAuthResult: () => false,
-  //   },
-  // }
-
-  // componentDidMount() {
-  //   firebase.auth().onAuthStateChanged(user => {
-  //     this.setState({ isSignedIn: !!user })
-  //   })
-  // }
-
+class App extends React.Component<IUser, {}> {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Auth} />
-          </Switch>
-          {/* {this.state.isSignedIn ? (
-            <div>Signed In</div>
-          ) : (
-            <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
-          )} */}
-        </div>
+        <Switch>
+          <Route exact path="/">
+            {this.props.inloggedUser ? <Home /> : <Auth />}
+          </Route>
+        </Switch>
       </BrowserRouter>
     )
   }
 }
 
-export default App
+const mapStateToProps = (state: any): IUser => {
+  return {
+    inloggedUser: state.firebase.auth.displayName,
+  }
+}
+
+export default compose<any>(
+  connect(
+    mapStateToProps,
+    null
+  )
+)(App)
--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -4,20 +4,8 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
-interface IState {
-  isSignedIn: boolean
-}
-
-class Auth extends React.Component<{}, IState> {
-  constructor(props: any) {
-    super(props)
-
-    this.state = {
-      isSignedIn: false,
-    }
-  }
-
-  uiConfig = {
+const Auth = () => {
+  const uiConfig = {
     signInFlow: 'popup',
     signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.FacebookAuthProvider.PROVIDER_ID],
     callbacks: {
@@ -25,29 +13,18 @@ class Auth extends React.Component<{}, IState> {
     },
   }
 
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user })
-    })
-  }
-
-  render() {
-    // console.log('Korv:', firebase.auth())
-    return (
-      <div>
-        <div className="login-container">
-          <div className="logo">Foodie</div>
-          <div className="content">
-            <p>We at foodie want you to find what you are looking for quickly and easily near you.</p>
-          </div>
-          <div className="signin-text">Sign in</div>
-          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
-          {/* {firebase.auth().currentUser.displayName } */}
-          {/* <p>{firebase.auth().currentUser && firebase.auth().currentUser.displayName}</p> */}
+  return (
+    <div>
+      <div className="login-container">
+        <div className="logo">Foodie</div>
+        <div className="content">
+          <p>We at foodie want you to find what you are looking for quickly and easily near you.</p>
         </div>
+        <div className="signin-text">Sign in</div>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Auth
