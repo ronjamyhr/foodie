de5036e9e9a452ee689a62ef26eea6901d9d76a6
ronjamyhr <ronja_myhr@hotmail.com>
2019-12-18 14:31:38 +0100

    add all places and style

--- src/components/Places/Places.tsx
+++ src/components/Places/Places.tsx
@@ -1,16 +1,46 @@
 import React from 'react'
 import './places.scss'
 import { NavLink } from 'react-router-dom'
+import { IFoodPlaces } from '../../types/FoodPlaces'
+import { AppState } from '../..'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import FilteredPlaces from './Filter/FilteredPlaces/FilteredPlaces'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSlidersH } from '@fortawesome/free-solid-svg-icons'
 
-const Places = () => {
+const Places = ({ places }: LinkStateProps) => {
   return (
     <div className="places-container">
-      <h1>Places</h1>
+      <h1 className="places-heading">PLACES</h1>
+
       <NavLink className="filter-link" exact to="/filter">
-        filtrera
+        <FontAwesomeIcon className="icon" icon={faSlidersH} />
+        <p className="text">FILTER</p>
       </NavLink>
+
+      <div className="places-wrapper">
+        {places && places.map(place => <div key={place.id}>{<FilteredPlaces result={place} />}</div>)}
+      </div>
     </div>
   )
 }
 
-export default Places
+interface LinkStateProps {
+  places: IFoodPlaces[]
+}
+
+const mapStateToProps = (state: AppState): LinkStateProps => {
+  return {
+    places: state.firestore.ordered.foodplaces,
+  }
+}
+
+export default compose<any>(
+  connect(
+    mapStateToProps,
+    null
+  ),
+  firestoreConnect([{ collection: 'foodplaces' }])
+)(Places)
